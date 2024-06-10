a2eb3abfa5bea0e8ce47c5ef4af8a1817093c8a5
Chris Shumaker <chris.shumaker@gmail.com>
2024-06-10 11:16:04 -0400

    Adding social security to retirement spend calc

--- site/shared/retirement.js
+++ site/shared/retirement.js
@@ -1,4 +1,4 @@
-function retirementBalances(principal, rate, years, withdrawal, inflation) {
+function retirementBalances(principal, rate, years, withdrawal, inflation, socialSecurity) {
 	let balances = [principal];
 	let balance = principal;
 
@@ -6,10 +6,11 @@ function retirementBalances(principal, rate, years, withdrawal, inflation) {
 		", Growth Rate: ", rate, 
 		", Balances: ", balances, 
 		", Withdrawal: ", withdrawal, 
-		", Inflation Rate: ", inflation)
+		", Inflation Rate: ", inflation, 
+		", Annual Social Security", socialSecurity)
 	
 	for (let i = 1; i<years.length; i++) {
-		balance = (balance - withdrawal) * (1 + rate);
+		balance = (balance - (withdrawal - socialSecurity)) * (1 + rate);
 		withdrawal = withdrawal * (1 + inflation);
 		balances.push(balance);
 	}
